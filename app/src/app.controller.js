3f79e2bbc8a074a8550d6f1f2befd9b6026def55
cartifon <ciceroartifon@gmail.com>
2017-10-02 16:43:40 -0400

    Starting with  font-awesome and bootstrap.
    Creating a grocery list

--- app/src/app.controller.js
+++ app/src/app.controller.js
@@ -3,9 +3,14 @@
         .controller('groceryController', GroceryController);
 
 
-    GroceryController.$inject = ['$scope'];
+    GroceryController.$inject = ['$scope', '$http'];
 
-    function GroceryController($scope) {
-        console.log('Let\'s do this');
+    function GroceryController($scope, $http) {
+        $http.get('../grocery-list.json').then( (response) => {
+            console.log(response.data);
+            // response.data.forEach( (item)=> {
+            //     console.log(item.name);
+            // });
+        });
     }
 })();
